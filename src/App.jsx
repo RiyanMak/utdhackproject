f92e4d8ac1f7885a3f064cd4dd219c7aee625928
Aarez Ali Momin <aarez.umatiya111@gmail.com>
2024-11-16 17:43:30 -0600

    3 sections css done

--- src/App.jsx
+++ src/App.jsx
@@ -17,17 +17,23 @@ function App() {
             <h2> X </h2>
             <h1 className='utdtitle'> UTDHackRipple </h1>
             </div>
-
+          <div className='ok'>
             <div className='advising'>
               <h1>Schedule an Advising Appointment</h1>
+              <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla ipsum doloribus odio, quae deserunt excepturi numquam eveniet, ipsa dolor nesciunt repudiandae dicta ipsam rem inventore nam. Perspiciatis illo eligendi veritatis?</h3>
+              <button className='advisingbutton'> Get Started </button>
             </div>
-            
+          </div>
             <div className='loan'>
               <h1>Need a loan but don't have enough credit?</h1>
+              <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla ipsum doloribus odio, quae deserunt excepturi numquam eveniet, ipsa dolor nesciunt repudiandae dicta ipsam rem inventore nam. Perspiciatis illo eligendi veritatis?</h3>
+              <button className='advisingbutton'> Get Started </button>
             </div>
             
             <div className='investment'>
               <h1>Investment Strategy Advising with AI ChatBot</h1>
+              <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla ipsum doloribus odio, quae deserunt excepturi numquam eveniet, ipsa dolor nesciunt repudiandae dicta ipsam rem inventore nam. Perspiciatis illo eligendi veritatis?</h3>
+              <button className='advisingbutton'> Get Started </button>
             </div>
           </body>
       </div>
