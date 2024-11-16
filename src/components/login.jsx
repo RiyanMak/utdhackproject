0844756b21a668da94065fd64b1c8ce7629787ae
Riyan Maknojia <86026506+RiyanMak@users.noreply.github.com>
2024-11-16 17:20:53 -0600

    added login box

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,5 +1,26 @@
 import React, { useState } from 'react';
+import './Login.css';
 
 function Login(){
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    return(
+        <div>
+            <Box style = {{backgroundColor: 'golden', padding: '20px', borderRadius: '10px'}}>
+                Box for login information
+            </Box>
+        </div>
+
+
+    )
+
+
+
+
+    // const handleUsername = (e) => {
+        
+    // }
+    
     
 }
