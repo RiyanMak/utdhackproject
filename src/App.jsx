6da6ec317910b8c6499c9e4a2e988ea18165daf0
Aarez Ali Momin <aarez.umatiya111@gmail.com>
2024-11-16 18:03:40 -0600

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
       <div>
         <header className='homepageheader'>
@@ -20,6 +16,24 @@ function App() {
             <h1 className='title'> Goldman Sachs </h1>
             <h2> X </h2>
             <h1 className='utdtitle'> UTDHackRipple </h1>
+            </div>
+          <div className='ok'>
+            <div className='advising'>
+              <h1>Schedule an Advising Appointment</h1>
+              <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla ipsum doloribus odio, quae deserunt excepturi numquam eveniet, ipsa dolor nesciunt repudiandae dicta ipsam rem inventore nam. Perspiciatis illo eligendi veritatis?</h3>
+              <button className='advisingbutton'> Get Started </button>
+            </div>
+          </div>
+            <div className='loan'>
+              <h1>Need a loan but don't have enough credit?</h1>
+              <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla ipsum doloribus odio, quae deserunt excepturi numquam eveniet, ipsa dolor nesciunt repudiandae dicta ipsam rem inventore nam. Perspiciatis illo eligendi veritatis?</h3>
+              <button className='advisingbutton'> Get Started </button>
+            </div>
+            
+            <div className='investment'>
+              <h1>Investment Strategy Advising with AI ChatBot</h1>
+              <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla ipsum doloribus odio, quae deserunt excepturi numquam eveniet, ipsa dolor nesciunt repudiandae dicta ipsam rem inventore nam. Perspiciatis illo eligendi veritatis?</h3>
+              <button className='advisingbutton'> Get Started </button>
             </div>
           </body>
       </div>
